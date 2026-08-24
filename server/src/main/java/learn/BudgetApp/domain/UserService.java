4f50efd37a9a3102e62c256f3975a01362e0f4f4
Abby <agamboa@dev-10.com>
2026-08-24 14:03:52 -0500

    Check for username rather than email

--- server/src/main/java/learn/BudgetApp/domain/UserService.java
+++ server/src/main/java/learn/BudgetApp/domain/UserService.java
@@ -1,6 +1,8 @@
 package learn.BudgetApp.domain;
 
+import learn.BudgetApp.models.User;
 import org.springframework.stereotype.Service;
+import learn.BudgetApp.data.UserRepository;
 
 @Service
 public class UserService {
@@ -15,9 +17,9 @@ public class UserService {
         return repository.findById(userId);
     }
 
-    public Result<User> authenticate(String email, String proposedPassword){
+    public Result<User> authenticate(String username, String proposedPassword){
         Result<User> result = new Result<>();
-        User existingUser = repository.findByEmail(email);
+        User existingUser = repository.findByUsername(username);
         if(existingUser == null){
             result.addErrorMessage("User not found", ResultType.NOT_FOUND);
             return result;
@@ -34,16 +36,16 @@ public class UserService {
 
     public Result<User> create(User user){
         Result<User> result = new Result<>();
-        if(user.getEmail().isBlank()){
-            result.addErrorMessage("Email cannot be blank", ResultType.INVALID);
+        if(user.getUsername().isBlank()){
+            result.addErrorMessage("Username cannot be blank", ResultType.INVALID);
         }
 
         if(user.getPassword().isBlank()){
             result.addErrorMessage("Password cannot be blank", ResultType.INVALID);
         }
 
-        if(repository.findByEmail(user.getEmail()) != null){
-            result.addErrorMessage("Email is already taken", ResultType.INVALID);
+        if(repository.findByUsername(user.getUsername()) != null){
+            result.addErrorMessage("Username is already taken", ResultType.INVALID);
         }
 
         if(result.isSuccess()){
