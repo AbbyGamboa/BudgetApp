11057e5923157d26f939a1c27d9a9df195d35753
Abby <agamboa@dev-10.com>
2026-08-24 14:00:36 -0500

    User service from pre capstone, expect changes

---
+++ server/src/main/java/learn/BudgetApp/domain/UserService.java
@@ -0,0 +1,57 @@
+package learn.BudgetApp.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User findByUserId(int userId){
+        return repository.findById(userId);
+    }
+
+    public Result<User> authenticate(String email, String proposedPassword){
+        Result<User> result = new Result<>();
+        User existingUser = repository.findByEmail(email);
+        if(existingUser == null){
+            result.addErrorMessage("User not found", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        if(existingUser.getPassword().equals(proposedPassword)){
+            result.setpayload(existingUser);
+        } else{
+            result.addErrorMessage("Incorrect password", ResultType.INVALID);
+        }
+
+        return result;
+    }
+
+    public Result<User> create(User user){
+        Result<User> result = new Result<>();
+        if(user.getEmail().isBlank()){
+            result.addErrorMessage("Email cannot be blank", ResultType.INVALID);
+        }
+
+        if(user.getPassword().isBlank()){
+            result.addErrorMessage("Password cannot be blank", ResultType.INVALID);
+        }
+
+        if(repository.findByEmail(user.getEmail()) != null){
+            result.addErrorMessage("Email is already taken", ResultType.INVALID);
+        }
+
+        if(result.isSuccess()){
+            User created = repository.create(user);
+            result.setpayload(created);
+        }
+
+        return result;
+
+    }
+}
