b384bba4e0c11c1008a95346dba5cc340cfcec91
Abby <agamboa@dev-10.com>
2026-08-24 09:58:17 -0500

    Setting up app and application.properties

---
+++ server/src/main/java/learn/BudgetApp/App.java
@@ -0,0 +1,9 @@
+package learn.BudgetApp;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
