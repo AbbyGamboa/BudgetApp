c01d2b41e6b25bffc7cb88f2894c355be333dc52
Abby <agamboa@dev-10.com>
2026-08-24 13:57:31 -0500

    Tested each UserRepository function

--- server/src/main/java/learn/BudgetApp/data/UserRepository.java
+++ server/src/main/java/learn/BudgetApp/data/UserRepository.java
@@ -3,12 +3,12 @@ package learn.BudgetApp.data;
 import learn.BudgetApp.models.User;
 
 public interface UserRepository {
-    public User findByEmail(String email) throws DataAccessException;
+    User findByEmail(String email) throws DataAccessException;
 
-    public User findByUsername(String username) throws DataAccessException;
+    User findByUsername(String username) throws DataAccessException;
 
-    public User create(User user) throws DataAccessException;
+    User create(User user) throws DataAccessException;
 
-    public User findById(int userId) throws DataAccessException;
+    User findById(int userId) throws DataAccessException;
 
 }
