dc7c69a0a002138a36f23634dd519dda4899ba18
Abby <agamboa@dev-10.com>
2026-08-25 15:54:52 -0500

    Start over with account

--- server/src/main/java/learn/BudgetApp/models/Account.java
+++ server/src/main/java/learn/BudgetApp/models/Account.java
@@ -1,42 +1,30 @@
 package learn.BudgetApp.models;
 
-import java.util.Objects;
-
 public class Account {
-    public int plaidAccountId;
-    public PlaidItems plaidItem;
-    public String name;
+    public int accountId;
+    public User user;
     public String subtype;
 
-    public Account(int plaidAccountId, PlaidItems plaidItem, String name, String subtype) {
-        this.plaidAccountId = plaidAccountId;
-        this.plaidItem = plaidItem;
-        this.name = name;
+    public Account(int accountId, User user, String subtype) {
+        this.accountId = accountId;
+        this.user = user;
         this.subtype = subtype;
     }
 
-    public int getPlaidAccountId() {
-        return plaidAccountId;
+    public int getAccountId() {
+        return accountId;
     }
 
-    public void setPlaidAccountId(int plaidAccountId) {
-        this.plaidAccountId = plaidAccountId;
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
     }
 
-    public PlaidItems getPlaidItem() {
-        return plaidItem;
+    public User getUser() {
+        return user;
     }
 
-    public void setPlaidItem(PlaidItems plaidItem) {
-        this.plaidItem = plaidItem;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public String getSubtype() {
@@ -45,17 +33,5 @@ public class Account {
 
     public void setSubtype(String subtype) {
         this.subtype = subtype;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return plaidAccountId == account.plaidAccountId && Objects.equals(plaidItem, account.plaidItem) && Objects.equals(name, account.name) && Objects.equals(subtype, account.subtype);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(plaidAccountId, plaidItem, name, subtype);
     }
 }
