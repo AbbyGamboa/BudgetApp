90dd29520667f2383e3f4a18e6150627d57e3c5d
Abby <agamboa@dev-10.com>
2026-08-30 10:58:08 -0500

    Can successfully output an http get request

--- server/src/main/java/learn/BudgetApp/controller/CategoryController.java
+++ server/src/main/java/learn/BudgetApp/controller/CategoryController.java
@@ -6,10 +6,7 @@ import learn.BudgetApp.domain.Result;
 import learn.BudgetApp.models.Category;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +28,18 @@ public class CategoryController {
         int userId = Integer.parseInt(authentication.getName());
 
         Result<List<Category>> result = service.findByUser(userId);
+        if (!result.isSuccess()){
+            return ErrorResponse.build(result);
+        }
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<?> findById(@PathVariable int categoryId, Authentication authentication){
+        int userId = Integer.parseInt(authentication.getName());
+
+        Result<Category> result = service.findById(categoryId, userId);
         if (!result.isSuccess()){
             return ErrorResponse.build(result);
         }
