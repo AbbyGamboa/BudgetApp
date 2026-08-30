c91fe3ed1917de7e5ac19ca0168f0e64b85148a5
Abby <agamboa@dev-10.com>
2026-08-29 19:48:49 -0500

    Service passes all tests

---
+++ server/src/main/java/learn/BudgetApp/domain/CategoryService.java
@@ -0,0 +1,43 @@
+package learn.BudgetApp.domain;
+
+import learn.BudgetApp.data.CategoryRepository;
+import learn.BudgetApp.data.UserRepository;
+import learn.BudgetApp.models.Category;
+import learn.BudgetApp.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository repository;
+    private final UserRepository userRepository;
+
+
+    public CategoryService(CategoryRepository repository, UserRepository userRepository) {
+        this.repository = repository;
+        this.userRepository = userRepository;
+    }
+
+    public Result<List<Category>> findByUser(int userId) {
+        Result<List<Category>> result = new Result<>();
+
+        User found = userRepository.findById(userId);
+        if (found == null) {
+            result.addErrorMessage("User not found", ResultType.NOT_FOUND);
+            return result;
+        }
+
+        List<Category> categories = repository.findAllCategoriesForUser(userId);
+        if(categories.isEmpty()){
+            result.addErrorMessage("User has no categories", ResultType.NOT_FOUND);
+        }
+
+        if(result.isSuccess()){
+            result.setpayload(categories);
+        }
+        return result;
+    }
+}
---
+++ server/src/test/java/learn/BudgetApp/domain/CategoryServiceTest.java
@@ -0,0 +1,71 @@
+package learn.BudgetApp.domain;
+
+import learn.BudgetApp.data.CategoryRepository;
+import learn.BudgetApp.data.TestDataHelper;
+import learn.BudgetApp.data.UserRepository;
+import learn.BudgetApp.models.Category;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class CategoryServiceTest {
+
+    @Autowired
+    CategoryService service;
+
+    @MockBean
+    CategoryRepository repository;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Nested
+    class findByUser{
+        @Test
+        void success(){
+            when(userRepository.findById(1)).thenReturn(TestDataHelper.existingUser());
+            when(repository.findAllCategoriesForUser(1)).thenReturn(TestDataHelper.categoriesForUserOne());
+
+
+            Result<List<Category>> expected = new Result<>();
+            expected.setpayload(TestDataHelper.categoriesForUserOne());
+            Result<List<Category>> actual = service.findByUser(1);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void failsWhenUserNotFound(){
+            when(userRepository.findById(99)).thenReturn(null);
+
+            Result<List<Category>> expected = new Result<>();
+            expected.addErrorMessage("User not found", ResultType.NOT_FOUND);
+            Result<List<Category>> actual = service.findByUser(99);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void failsWhenCategoryIsEmpty(){
+            when(userRepository.findById(1)).thenReturn(TestDataHelper.existingUser());
+            when(repository.findAllCategoriesForUser(1)).thenReturn(List.of());
+
+
+            Result<List<Category>> expected = new Result<>();
+            expected.addErrorMessage("User has no categories", ResultType.NOT_FOUND);
+            Result<List<Category>> actual = service.findByUser(1);
+
+            assertEquals(expected, actual);
+        }
+    }
+
+
+}
