2c3a2939a55606c4f39e648b64a983bbbe55350d
Abby <agamboa@dev-10.com>
2026-09-07 10:14:07 -0500

    Landing page

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -9,7 +9,10 @@ function Landing(){
 
     return (
         <>
-            <img src={background} alt="" className="w-100" style={{ height: "200px", objectFit: "cover" }}/>
+            <div className="background-blue w-100 d-flex" style={{ height: "200px", objectFit: "cover" }}>
+                <h1 className="mt-auto mb-1 ms-5">Take your first steps today...</h1>
+            </div>
+            
             <div className="d-flex justify-content-between m-5">
                 <Article image ={studentLoans} title={"Student Loans and Debt"} text={"Types of loans and understanding debt"}></Article>
                 <Article image={creditScore} title={"Credit and Credit Scores"} text={"What is credit and how to improve your score"}></Article>
